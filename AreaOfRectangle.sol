4606fe9afff0a7a219e9d0f4ecd135405d7e7e0f
reddynithya <110446728+reddynithya@users.noreply.github.com>
2024-03-29 11:47:54 +0530

    Update AreaOfRectangle.sol

--- AreaOfRectangle.sol
+++ AreaOfRectangle.sol
@@ -1,1 +1,15 @@
+pragma solidity ^0.8.14;
+
+contract Rectangle {
+    // Define a struct to represent the rectangle
+    struct Rectangle {
+        uint256 length;
+        uint256 width;
+    }
+    
+    // Function to calculate the area of a rectangle
+    function calculateArea(uint256 _length, uint256 _width) public pure returns (uint256) {
+        return _length * _width;
+    }
+}
 
